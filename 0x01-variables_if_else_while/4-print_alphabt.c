97777b166f5d101c2c2362da0508c176181e04a3
3oss <youssofothman89@gmail.com>
2023-06-16 15:06:09 -0700

    l

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing all alphabets except q and e
+ *
+ * Return: Always  0(success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+
+	if (ch == 'q' || ch != 'e')
+		ch++;
+
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
