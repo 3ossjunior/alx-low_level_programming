7f05380351dca103c6c872fb4fa089f9cd1124d3
3oss <youssofothman89@gmail.com>
2023-06-22 19:09:50 -0700

    a

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -47,3 +47,16 @@ void largest_prime_factor(long int num)
 	la = num;
 	 printf("%d", la);
 }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	largest_prime_factor(612852475143);
+
+	return (0);
+}
+
