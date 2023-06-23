5b007cdba126d2a31dd27c5355d13ff67da34df6
3oss <youssofothman89@gmail.com>
2023-06-22 20:03:10 -0700

    a

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,10 +13,10 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		n = -n;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	if ((n / 10) > 0)
+		print_number(n / 10);
 	_putchar((num % 10) + 48);
 }
