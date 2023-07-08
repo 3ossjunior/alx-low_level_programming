942e0c426146a47bfb1eaec03da6ff3504380402
3oss <youssofothman89@gmail.com>
2023-07-07 19:40:13 -0700

    m

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,6 @@
  * @argc:number of commandline arguments
  * @argv:pointer to an array of command line arguments
  * Return:0-success,
- * non-zero-fail.
  */
 
 int main(int argc, char *argv[])
@@ -15,7 +14,7 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		sum = atoi(argc[1]) * atoi(argv[2]);
+		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
 	}
 	else
@@ -23,5 +22,5 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	return (0);
+	return (0);
 }
